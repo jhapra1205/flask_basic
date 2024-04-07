1eb67fc6a20aabc9a94cd0b85fb9d5019716b9b1
jhapra1205 <jhapra1205@gmail.com>
2024-04-07 12:32:30 -0500

    first commit

---
+++ application.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+application = Flask(__name__)
+response = list()
+
+
+@application.route("/")
+def say_hello():
+    return jsonify("Welcome to page deployed using AWS ECS")
+
+
+@application.route("/name")
+def say_hello_from_request():
+    name = request.json.get("name")
+    return jsonify("my name is " + name)
+
+
+@application.route("/adddata", methods=["POST"])
+def add_data():
+    data = dict()
+    name = request.json.get("name")
+    age = request.json.get("age")
+    data["name"] = name
+    data["age"] = age
+    response.append(data)
+    return jsonify(response)
---
+++ run.py
@@ -0,0 +1,2 @@
+from application import application
+application.run(debug=True, host='0.0.0.0', port=5000)
